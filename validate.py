949fda5d2b5332fbe54543218c1955a57f92e0c0
ishfulness <malmccar@reed.edu>
2018-04-05 16:01:23 -0700

    Testing second commit

---
+++ validate.py
@@ -0,0 +1,26 @@
+
+
+# Check to see:
+# All universes have correct outer diameter
+# no fuel element is used more than once
+
+
+# Other anomolies????????
+
+# 
+# print number of graphite 
+# print picture of core?
+# Take in hmap whatever
+
+# make the heatmap generator spit out unused elements also 
+
+
+
+# don't generate, validate the file itself
+
+# print control rod transformations?
+
+
+
+
+# Another test
